db1249ec04adabd70e61e4224dca9afb80753594
Pojok Code <asep.komarudin@dataon.com>
2024-11-16 10:09:40 +0700

    fix return only file type java

--- lua/auto-lsp/lsp/settings/jdtls.lua
+++ lua/auto-lsp/lsp/settings/jdtls.lua
@@ -1,22 +1,26 @@
-return {
-  cmd = {
-    "jdtls",
-    "-configuration",
-    vim.fn.stdpath("cache") .. "/jdtls/config",
-    "-data",
-    vim.fn.stdpath("cache") .. "/jdtls/workspace",
-  },
-  filetypes = { "java" },
-  root_dir = require("lspconfig.util").root_pattern(
-    -- Single-module projects
-    {
-      "build.xml", -- Ant
-      "pom.xml", -- Maven
-      "settings.gradle", -- Gradle
-      "settings.gradle.kts", -- Gradle
+if vim.bo.filetype ~= "java" then
+  return {
+    cmd = {
+      "jdtls",
+      "-configuration",
+      vim.fn.stdpath("cache") .. "/jdtls/config",
+      "-data",
+      vim.fn.stdpath("cache") .. "/jdtls/workspace",
     },
-    -- Multi-module projects
-    { "build.gradle", "build.gradle.kts" }
-  ) or vim.fn.getcwd(),
-  singe_file_support = true,
-}
+    filetypes = { "java" },
+    root_dir = require("lspconfig.util").root_pattern(
+      -- Single-module projects
+      {
+        "build.xml", -- Ant
+        "pom.xml", -- Maven
+        "settings.gradle", -- Gradle
+        "settings.gradle.kts", -- Gradle
+      },
+      -- Multi-module projects
+      { "build.gradle", "build.gradle.kts" }
+    ) or vim.fn.getcwd(),
+    singe_file_support = true,
+  }
+else
+  return {}
+end
