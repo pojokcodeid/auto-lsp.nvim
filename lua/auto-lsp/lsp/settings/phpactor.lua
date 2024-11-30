2ac03b8faa5ce2bd36e05880319d0768d98e6ed9
Pojok Code <asep.komarudin@dataon.com>
2024-11-30 22:41:55 +0700

    add: phpactor config

---
+++ lua/auto-lsp/lsp/settings/phpactor.lua
@@ -0,0 +1,6 @@
+return{
+  cmd = { "phpactor", "language-server" },
+  filetypes = { "php" },
+  root_dir = require("lspconfig.util").root_pattern("composer.json", ".git",'.phpactor.json', '.phpactor.yml') or vim.loop.cwd() or vim.fn.getcwd(),
+  single_file_support = true
+}
