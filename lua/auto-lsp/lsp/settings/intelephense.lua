b898a6b6eaa0c93c592bd39bf83b2ab1e523317b
Pojok Code <asep.komarudin@dataon.com>
2024-12-03 08:19:09 +0700

    enc: add support php 8.4

--- lua/auto-lsp/lsp/settings/intelephense.lua
+++ lua/auto-lsp/lsp/settings/intelephense.lua
@@ -1,6 +1,25 @@
-return {
+local M = {
 	cmd = { "intelephense", "--stdio" },
 	filetypes = { "php" },
 	root_dir = require("lspconfig.util").root_pattern("composer.json", ".git") or vim.loop.cwd() or vim.fn.getcwd(),
 	single_file_support = true,
 }
+
+local function validate_php_version()
+	local handle = io.popen("php -v")
+	local result = handle:read("*a")
+	handle:close()
+
+	if not string.match(result, "PHP 8.4.") then
+			return false
+	end
+	return true
+end
+
+if validate_php_version() then
+	M.settings = {
+			phpVersion = "8.4.0",  -- Anda bisa menyesuaikan versi ini sesuai kebutuhan
+	}
+end
+
+return M
