87c16c87eef5a94472f88f87361ed14f412d40fc
Pojok Code <pojokcodeid@gmail.com>
2025-07-14 23:18:36 +0700

    enc: add emmetls for php file

---
+++ lua/auto-lsp/lsp/settings/emmet_ls.lua
@@ -0,0 +1,22 @@
+return {
+  cmd = { "emmet-ls", "--stdio" },
+  filetypes = {
+    "astro",
+    "css",
+    "eruby",
+    "html",
+    "htmlangular",
+    "htmldjango",
+    "javascriptreact",
+    "less",
+    "pug",
+    "sass",
+    "scss",
+    "svelte",
+    "templ",
+    "typescriptreact",
+    "vue",
+    "php",
+  },
+  root_markers = { ".git" },
+}
